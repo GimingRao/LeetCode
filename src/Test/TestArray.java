c2d44727d25e39e93e1a87e4c787ab688bfa704b
giming <2425198313@qq.com>
2023-02-20 20:07:12 +0800

    LRU+逆波兰数

--- src/Test/TestArray.java
+++ src/Test/TestArray.java
@@ -2,13 +2,14 @@ package Test;
 
 import Topic.Topic134;
 import Topic.Topic15;
+import Topic.Topic150;
 
 public class TestArray {
     public  static int[]TestGas=new int[]{3,1,1};
     public  static int[]TestCost=new int[]{1,2,2};
 
     public static void main(String[] args) {
-        Topic134 testUnit=new Topic134();
-        System.out.println(testUnit.canCompleteCircuit(TestGas, TestCost));
+        Topic150 testUnit=new Topic150();
+        System.out.println(testUnit.evalRPN(new String[]{"10","6","9","3","+","-11","*","/","*","17","+","5","+"}));
     }
 }
